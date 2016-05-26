99e1bccb7bcfe8e8d8b3b5d592ebba87a950b398
nisanth <nisanth.yaganti@gmail.com>
2016-05-26 13:53:41 -0400

    removed wait for ajax call

--- features/step_definitions/brokers_steps.rb
+++ features/step_definitions/brokers_steps.rb
@@ -112,8 +112,8 @@ end
 
 Then(/^.+ should see broker agencies index view$/) do
   #TODO add AJAX handling
-  wait_for_ajax(3)
-  expect(page).to have_content('Broker Agencies', :wait => 5)
+  #wait_for_ajax(3)
+  expect(page).to have_content('Broker Agencies')
 end
 
 When(/^.+ searches broker agency by name$/) do
