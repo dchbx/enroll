5993766c63ae5d63b9f76deb44028ece4e39ecb0
Battula <vrbattula@outlook.com>
2016-09-16 16:06:17 -0400

    refs #10655 eliminate both terminated and cancelled policies in glue

--- script/2016_policies_report_skip_terminated_policies_from_glue.rb
+++ script/2016_policies_report_skip_terminated_policies_from_glue.rb
@@ -1,16 +1,16 @@
 require 'csv'
 
-csv_glue = CSV.read("/Users/Varun/Desktop/reports/10655_export_glue_multi_row_sep_16.csv", :headers => true)
+csv_glue = CSV.read("/Users/Varun/Desktop/reports/10655_export_glue_multi_row_sep_16_cancelled.csv", :headers => true)
 csv_ea = CSV.read("/Users/Varun/Desktop/reports/10655_export_ea_multirow_sep_15.csv", :headers => true)
 
-csv_match = CSV.open("glue_terminated_poilices.csv", "w")
+csv_match = CSV.open("glue_canceled_and_terminated_poilices.csv", "w")
 csv_match << %w(family.id policy.id policy.subscriber.coverage_start_on policy.aasm_state policy.plan.coverage_kind policy.plan.metal_level policy.subscriber.person.hbx_id policy.subscriber.person.is_incarcerated  policy.subscriber.person.citizen_status policy.subscriber.person.is_dc_resident?  is_dependent)
 
-csv_mismatch = CSV.open("final_ea_report.csv", "w")
+csv_mismatch = CSV.open("final_ea_report_sep_16.csv", "w")
 csv_mismatch << %w(family.id  policy.id policy.subscriber.coverage_start_on policy.aasm_state policy.plan.coverage_kind policy.plan.metal_level policy.subscriber.person.hbx_id policy.subscriber.person.is_incarcerated  policy.subscriber.person.citizen_status policy.subscriber.person.is_dc_resident?  is_dependent)
 
 terminated_glue_keys=csv_glue.select do |row| 
-  row["policy.aasm_state"].downcase == 'terminated' 
+  row["policy.aasm_state"].downcase == 'terminated' || row["policy.aasm_state"].downcase == 'canceled'
 end.flat_map{|r| r["policy.eg_id"]+"_"+r["person.authority_member_id"] }
 
 csv_ea.each do |row|
