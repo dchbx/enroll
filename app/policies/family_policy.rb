22aee68b0f43855779847af0b5792c87bdc8d7cc
jfinucane <jimway@gmail.com>
2016-09-20 17:04:02 -0400

    Refs #10549 add can_edit_ssn permission to permission model

--- app/policies/family_policy.rb
+++ app/policies/family_policy.rb
@@ -1,12 +1,12 @@
 class FamilyPolicy < ApplicationPolicy
   def updateable?
-  	return true unless role = user.person && user.person.hbx_staff_role
+    return true unless role = user.person && user.person.hbx_staff_role
     role.permission.modify_family
   end
-  
+
   def can_update_ssn?
-  	return false unless role = user.person && user.person.hbx_staff_role
-    role.permission_id  = Permission.hbx_staff.id
+    return false unless role = user.person && user.person.hbx_staff_role
+    role.permission.can_update_ssn
    end
 
 end
