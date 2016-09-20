7fd4c1dab026e576deef14624e431661e3e03988
Battula <vrbattula@outlook.com>
2016-09-20 11:04:24 -0400

    refs #10655 compare policy.aasm_state from enroll and glue report

--- script/diff_ea_glue_policies.rb
+++ script/diff_ea_glue_policies.rb
@@ -11,17 +11,17 @@ def csv_to_hash(csv)
 end
 
 def person_field_array(row)
-  row.values_at(6, 7, 8, 9)
+  row.values_at(3)
 end
 
-csv_glue = CSV.read("/Users/Varun/Desktop/reports/10655_export_glue_multi_row.csv")
-csv_ea = CSV.read("/Users/Varun/Desktop/reports/10655_export_ea_multirow_9_13.csv")
+csv_glue = CSV.read("/Users/Varun/Desktop/reports/10655_export_glue_multi_row_no_csr_no_aptc.csv")
+csv_ea = CSV.read("/Users/Varun/Desktop/reports/10655_export_ea_multirow_sep_19_change_status.csv")
 
-csv_match = CSV.open("matching_people.csv", "w")
-csv_match << %w(family_id policy_eg_id glue_person.hbx_id glue_incarcerated glue_citizenship glue_dc_resident ea_person.hbx_id, ea_incarcerated, ea_citizenship ea_dc_resident)
+csv_match = CSV.open("matching_people_sep_19_change_status.csv", "w")
+csv_match << %w(family_id glue_eg_id ea_eg_id glue_aasm_state ea_aasm_state glue_hbx_id ea_hbx_id glue_incarcerated glue_citizenship glue_dc_resident ea_incarcerated ea_citizenship ea_dc_resident)
 
-csv_mismatch = CSV.open("mismatching_people.csv", "w")
-csv_mismatch << %w(family_id policy_eg_id glue_person.hbx_id glue_incarcerated glue_citizenship glue_dc_resident ea_person.hbx_id, ea_incarcerated, ea_citizenship ea_dc_resident)
+csv_mismatch = CSV.open("mismatching_people_sep_19_change_status.csv", "w")
+csv_mismatch << %w(family_id glue_eg_id ea_eg_id glue_aasm_state ea_aasm_state glue_hbx_id ea_hbx_id glue_incarcerated glue_citizenship glue_dc_resident ea_incarcerated ea_citizenship ea_dc_resident)
 
 
 glue_hash = csv_to_hash(csv_glue)
@@ -31,10 +31,8 @@ ea_hash = csv_to_hash(csv_ea)
 glue_hash.each do |key, row|
   next if ea_hash[key].nil?
   if (person_field_array(row) <=> person_field_array(ea_hash[key])) != 0
-    csv_mismatch << [ea_hash[key][0]] +  [row[1]] + person_field_array(row) + person_field_array(ea_hash[key])
+    csv_mismatch << [ea_hash[key][0]] + [row[1]] + [ea_hash[key][1]] + [row[3]] + [ea_hash[key][3]] + [row[6]] + [ea_hash[key][6]] + [row[7]] + [row[8]] + [row[9]] + [ea_hash[key][7]] + [ea_hash[key][8]] + [ea_hash[key][9]]
   else
-    csv_match << [ea_hash[key][0]] + [row[1]] + person_field_array(row) + person_field_array(ea_hash[key])
+    csv_match << [ea_hash[key][0]] + [row[1]] + [ea_hash[key][1]] + [row[3]] + [ea_hash[key][3]] + [row[6]] + [ea_hash[key][6]] + [row[7]] + [row[8]] + [row[9]] + [ea_hash[key][7]] + [ea_hash[key][8]] + [ea_hash[key][9]]
   end
 end
-
-
--- script/skip_unwanted_fields_for_ivlr1_notices.rb
+++ script/skip_unwanted_fields_for_ivlr1_notices.rb
@@ -1,11 +1,7 @@
 require 'csv'
 
-# csv_ea = CSV.read("/Users/Varun/Documents/DCHBX/enroll/matching_people_sep_19_change_status_copy.csv", :headers => true)
 csv_match = CSV.open("/Users/Varun/Desktop/reports/sep_20/final_report_sep_20.csv", "w")
 csv_match << %w(family_id	glue_eg_id	ea_eg_id	glue_aasm_state	ea_aasm_state	glue_hbx_id	ea_hbx_id	glue_incarcerated	glue_citizenship	glue_dc_resident	ea_incarcerated	ea_citizenship	ea_dc_resident)
-
-# csv_mismatch = CSV.open("final_report_2.csv", "w")
-# csv_mismatch << %w(family_id	glue_eg_id	ea_eg_id	glue_aasm_state	ea_aasm_state	glue_hbx_id	ea_hbx_id	glue_incarcerated	glue_citizenship	glue_dc_resident	ea_incarcerated	ea_citizenship	ea_dc_resident)
 
 begin
 	csv = CSV.open('/Users/Varun/Desktop/matching_people_sep_19_change_status_term_canceled.csv',"r",:headers =>true)
@@ -23,7 +19,6 @@ begin
   unwanted_families = []
   @data_hash.each do |family_id,members|
     members.each do |member|
-      # binding.pry
       if member["ea_incarcerated"].nil? || member["ea_incarcerated"] == "TRUE" || member["ea_citizenship"].nil? || member["ea_dc_resident"].nil? || member["ea_citizenship"] == "non_native_not_lawfully_present_in_us" || member["ea_citizenship"] == "not_lawfully_present_in_us" || member["ea_citizenship"] == "lawful_permanent_resident" || member["ea_dc_resident"] == "FALSE"
         unwanted_families << family_id
       end
@@ -34,7 +29,6 @@ begin
 
   @data_hash.each do |family_id,members|
     members.each do |member|
-      # binding.pry
       csv_match.add_row(member)
     end
   end
@@ -42,5 +36,3 @@ begin
 rescue Exception => e
   puts "Unable to open file #{e} #{e.backtrace}"
 end
-
-# next if x[4] == "terminated" || x[4] == "canceled" ||  x[10] == nil || x[10] == "TRUE" || x[11] == nil || x[11] == "non_native_not_lawfully_present_in_us" || x[11] == "not_lawfully_present_in_us" || x[11] == "lawful_permanent_resident" || x[12] == "FALSE"
