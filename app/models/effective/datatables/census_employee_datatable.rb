7e533d7c7a45c750fc8ed2566cb6fb1a9ea18cb0
Ji Wang <traceyw0224@gmail.com>
2016-11-10 11:40:25 -0500

    modified format

--- app/models/effective/datatables/census_employee_datatable.rb
+++ app/models/effective/datatables/census_employee_datatable.rb
@@ -3,10 +3,12 @@ module Effective
     class CensusEmployeeDatatable < Effective::MongoidDatatable
 
       datatable do
-        #default_order :full_name, :asc
+        default_order :full_name, :asc
         default_entries 10
-        #table_column :fein, :label => 'FEIN', :proc => Proc.new { |row| row.fein }, :sortable => false, :filter => false
-        table_column :full_name, :label => 'Full Name', :width =>'100px',:proc => Proc.new { |row| row.full_name },  :sortable => true,:filter => false
+        table_column :full_name, :label => 'Full Name', :width =>'100px',
+                     :proc => Proc.new { |row| link_to row.full_name.titleize,
+                                                       employers_employer_profile_census_employee_path(row.employer_profile_id, row.id, status: row.aasm_state)},
+                     :sortable => true,:filter => false
         table_column :dob,:label => 'Date of Birth',:width =>'100px',:proc => Proc.new { |row| row.dob },:sortable => false, :filter => false
         table_column :aasm_state,:label => 'Status',:width => '100px',:proc => Proc.new { |row| employee_state_format(row.aasm_state, row.employment_terminated_on)},:sortable => true,:filter => false
         table_column :hired_on,:label => 'Hired on',:width => '100px',:proc => Proc.new { |row| row.hired_on },:sortable => true,:filter => false
@@ -24,46 +26,11 @@ module Effective
         if !attributes[:current_employer_profile].nil? && attributes[:current_employer_profile].renewing_published_plan_year.present?
            table_column :renewal_enrollment_status, :label=>"Renewal Enrollment Status"
         end
+
         table_column :action do |row|
           edit(row)
           terminate(row)
-
         end
-
-
-      #
-      #   <td>
-      #   <%= link_to name_to_listing(census_employee), employers_employer_profile_census_employee_path(@employer_profile.id, census_employee.id, status: status) %>
-      #     </td>
-      #     <td><%= format_date census_employee.dob %></td>
-      #
-      #   </td>
-      #     <% if @employer_profile.renewing_plan_year.present? %>
-      #
-      #     </td>
-      #   <% end %>
-      # <% if ['terminated', 'all'].include?(status) %>
-      # <td><%= format_date census_employee.employment_terminated_on %>
-      #     </td>
-      #     <% end %>
-      #     <td><%= enrollment_state(census_employee) %></td>
-      #     <% if @employer_profile.renewing_published_plan_year.present? %>
-      #         <td><%= renewal_enrollment_state(census_employee) %></td>
-      # <% end %>
-      # <td class="table-functions">
-      # <% if census_employee.employment_terminated? %>
-      #       <a class="show_confirm btn btn-green-2" id="show_confirm_#{census_employee.id} <%= pundit_class EmployerProfile, :updateable? %>">
-      #         <span class="hidden-xs hidden-sm visible-md visible-lg" style="margin-left: -10px;" onclick="$(this).closest('tr').next().show();">Rehire</span>
-      #       </a>
-      #     <% else %>
-      #     <%= link_to raw('<i class="fa fa-pencil fa-lg pull-right" data-toggle="tooltip" title="Edit"></i>'), employers_employer_profile_census_employee_path(@employer_profile.id, census_employee.id, status: status, tab: 'employees') %>
-      #       <i class="fa fa-trash-o fa-lg pull-right" data-toggle="tooltip" title="Terminate"></i>
-      #     <% end %>
-      #     </td>
-
-
-
-
       end
 
       def renew_benefit_group(row)
@@ -71,12 +38,13 @@ module Effective
           row.renewal_benefit_group_assignment.benefit_group.title.capitalize
         end
       end
+
       def edit(row)
 
       end
+
       def terminate(row)
         if row.employment_terminated?
-
         else
           #link_to raw('<i class="fa fa-pencil fa-lg pull-right" data-toggle="tooltip" title="Edit"></i>'), employers_employer_profile_census_employee_path(@employer_profile.id, census_employee.id, status: census_employee.aasm_state, tab: 'employees')
         end
@@ -89,7 +57,6 @@ module Effective
       scopes do
         scope :legal_name, "Hello"
       end
-
 
       def global_search?
         true
