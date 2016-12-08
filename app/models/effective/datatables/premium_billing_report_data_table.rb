6f6c8d7692516adfc2af46cb5456bfae1df6b6f2
PRANAB <pranab.khanal@gmail.com>
2016-12-08 12:43:48 -0500

    Refs#5434 Fixed date parsing in the collection method and styling

--- app/models/effective/datatables/premium_billing_report_data_table.rb
+++ app/models/effective/datatables/premium_billing_report_data_table.rb
@@ -64,12 +64,12 @@ module Effective
 
            def collection
             @employer_profile = EmployerProfile.find(attributes[:id])
-            billing_date = attributes[:billing_date]
-            @hbx_enrollments = @employer_profile.enrollments_for_billing(billing_date)
+            billing_date = (attributes[:billing_date].is_a? Date) ? attributes[:billing_date] : Date.parse(attributes[:billing_date])
+            @hbx_enrollments = @employer_profile.enrollments_for_billing(billing_date).select{|e|e.employee_role}
           end
 
           def global_search?
-            false
+            true
           end
 
         end
