deecf2881e53a404bada2f6dfd7b94e9eeada5e3
Battula <vrbattula@outlook.com>
2016-12-14 09:45:55 -0500

    refs #12694 generate invoice for ICON INC employer

--- app/helpers/employers/employer_helper.rb
+++ app/helpers/employers/employer_helper.rb
@@ -63,11 +63,11 @@ module Employers::EmployerHelper
 
 
   def invoice_formated_date(date)
-    date.strftime("%m/%d/%Y")
+    (date-2.days).strftime("%m/%d/%Y")
   end
 
   def invoice_coverage_date(date)
-    "#{date.next_month.beginning_of_month.strftime('%b %Y')}" rescue nil
+    "#{date.beginning_of_month.strftime('%b %Y')}" rescue nil
   end
 
   def coverage_kind(census_employee=nil)
