09a9d23b37d97a5049ae6ab96719619f1c736290
Jayanth <jayanth.rordev@gmail.com>
2017-03-13 10:17:37 -0400

    comment out some lines

--- app/data_migrations/update_invalid_benefit_group_assignments_for_employer.rb
+++ app/data_migrations/update_invalid_benefit_group_assignments_for_employer.rb
@@ -11,14 +11,10 @@ class UpdateInvalidBenefitGroupAssignmentsForEmployer < MongoidMigrationTask
       return
     end
     ces = organizations.first.employer_profile.census_employees.map(&:id)
-    # puts "@@@@@@@@--- #{ces}"
     for i in 0..(ces.length - 1)
       ce = CensusEmployee.find(ces[i])
-      # puts ce.inspect
       benefit_group_assignments = ce.benefit_group_assignments
-      # puts ce.benefit_group_assignments.count
       benefit_group_assignments.each do |bga|
-        # puts bga.inspect, bga.valid?
        if !bga.valid?
          if bga.hbx_enrollment && bga.hbx_enrollment.benefit_group.present?
            puts "Updating benefit group id on enrollment for #{ce.first_name} #{ce.last_name}"
