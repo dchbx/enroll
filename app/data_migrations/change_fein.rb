1976f05c92be2105e705b97ac741b02f8afc21fc
Ji Wang <traceyw@vt.edu>
2017-04-13 14:00:25 -0400

    Ref#15002 update the fein to deal with the duplication fein case

--- app/data_migrations/change_fein.rb
+++ app/data_migrations/change_fein.rb
@@ -6,6 +6,8 @@ class ChangeFein< MongoidMigrationTask
     new_fein = ENV['new_fein']
     if organization.nil?
       puts "No organization was found by the given fein"
+    elsif Organization.where(fein:new_fein).size >0
+      puts "Existing other organization with the same #{new_fein}"
     else
       organization.update_attributes(fein:new_fein)
       puts "Changed fein to #{new_fein}" unless Rails.env.test?
