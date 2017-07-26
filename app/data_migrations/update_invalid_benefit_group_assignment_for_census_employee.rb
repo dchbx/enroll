3b7d34d8e4c3e51a8b39d6c9e5fb79859fc92ee4
Jayanth <jayanth.rordev@gmail.com>
2017-07-26 17:19:52 -0400

    refs#15494 Refactor code

--- app/data_migrations/update_invalid_benefit_group_assignment_for_census_employee.rb
+++ app/data_migrations/update_invalid_benefit_group_assignment_for_census_employee.rb
@@ -6,9 +6,7 @@ class UpdateInvalidBenefitGroupAssignmentForCensusEmployee < MongoidMigrationTas
       census_employee = CensusEmployee.where(_id: ENV["census_employee_id"].to_s).first
       census_employee.benefit_group_assignments.each do |bga|
         if bga.is_active? && !bga.valid?
-          bga.update_attribute(:aasm_state, 'coverage_expired')
-          bga.update_attribute(:is_active, false)
-          bga.update_attribute(:hbx_enrollment_id, nil)
+          bga.update_attributes!(:aasm_state, 'coverage_expired', :is_active, false, :hbx_enrollment_id, nil)
           puts "Updated Benefit group assignment for Employee with id: #{employee_role_id}" unless Rails.env.test?
         end
       end   
