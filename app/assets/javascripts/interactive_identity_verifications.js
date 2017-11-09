eb07772f24affb7d513733e54661e63143b4a400
202chaz <chaz.haskins@gmail.com>
2017-11-09 04:07:43 -0500

    removed let

--- app/assets/javascripts/interactive_identity_verifications.js
+++ app/assets/javascripts/interactive_identity_verifications.js
@@ -1,13 +1,10 @@
-$(document).ready(function() {
-  
-  $('.v-type-status').each(function(i) {
-    let value = $(this).text().replace(/\s/g, "") ;
-
+$('.outstanding-ridp-documents').ready(function() {
+  $('.v-type-status').each(function() {
+    var value = $(this).text().replace(/\s/g, "");
     if (value != "Verified") {
       $('#btn-continue').addClass('blocking');
     }
   });
-  
   $('.fa-trash-o').click(function() {
     location.reload(true);
   })
