30c28afaeb9a10deb202ffd003f04774dfa7cce2
harsha280 <harsha.vskr@gmail.com>
2018-03-27 14:00:52 -0400

    Refs 23311 23313 23312 23316 sites controller methods

--- components/benefit_sponsors/app/controllers/benefit_sponsors/sites_controller.rb
+++ components/benefit_sponsors/app/controllers/benefit_sponsors/sites_controller.rb
@@ -1,6 +1,26 @@
 module BenefitSponsers
   class SitesController < ApplicationController
+    #before_action :find_site, only: [:show, :edit, :update, :destroy]
+
     def index
     end
+
+    def new
+      @site = Site.new
+    end
+
+    def create
+      redirect_to action: 'index'
+    end
+
+    def edit
+      @site = Site.find(params[:id]) # remove this and move to private method after writing show/edit/update/destory method. As per DRY
+    end
+
+    # private
+    #
+    # def find_site
+    #   @site = Site.find(params[:id])
+    # end
   end
 end
