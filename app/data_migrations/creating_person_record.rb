b9d1291996002b96de90958bc590ad2f25b8393a
Abhinav Darisipudi <abhinavdarisipudi@ip-192-168-254-10.ec2.internal>
2018-08-07 10:40:22 -0400

    27965: added address to person record

--- app/data_migrations/creating_person_record.rb
+++ app/data_migrations/creating_person_record.rb
@@ -5,13 +5,17 @@ class CreatingPersonRecord < MongoidMigrationTask
     file_name = ENV['file_name'].to_s
     index = 1
     CSV.foreach("#{Rails.root}/#{file_name}", headers: true) do |row|
-      person = Person.new(first_name: row[0], middle_name: row['Middle name'], last_name: row['Last name'],
-                          dob: Date.parse(row['DOB']), ssn: row['SSN'], gender: row['Gender'], hbx_id: row['hbx_id'])
-      if person.save
+      begin
+        person = Person.where(ssn: row['SSN']).first_or_create(first_name: row[0], middle_name: row['Middle name'], last_name: row['Last name'],
+                            dob: Date.parse(row['DOB']), gender: row['Gender'], hbx_id: row['hbx_id'])
+      
+
+        address = Address.new(address_1: row['address 1'], address_2: row['address 2'], city: row['city'], state: row['state'], zip: row['zip'], kind: "home")
+        
+        person.addresses << address
         puts "Person record created for row #{index}." unless Rails.env.test?
-      else
-        puts person.errors.full_messages
-        puts "Person record doesn't created for row #{index}" unless Rails.env.test?
+      rescue Exception => e
+        puts e
       end
       index += 1
     end
