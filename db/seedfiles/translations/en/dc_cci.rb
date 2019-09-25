1643c0c77f6b3e6e13d3d4a5395f1aeb93bcaaac
vineel <sonnathivinil@gmail.com>
2019-09-25 05:01:10 -0600

    54418: Fix carrier contact information for DC

---
+++ db/seedfiles/translations/en/dc_cci.rb
@@ -0,0 +1,62 @@
+PLAN_TRANSLATIONS = {
+  'en.dc_cci.csl' => 'Customer Service Line',
+  'en.dc_cci.call' => 'Call:',
+  'en.dc_cci.tty' => 'TTY:',
+  'en.dc_cci.tty_number' => '711 TTY',
+  'en.dc_cci.tty_num' => '711',
+  'en.dc_cci.or' => 'or',
+
+  'en.dc_cci.aetna_brokers_and_emprs' => 'For Brokers and Employers',
+  'en.dc_cci.aetna_broker_and_emprs_phone' => '1-855-319-7290',
+  'en.dc_cci.aetna_href_broker_and_emprs_phone' => 'tel:+18553197290',
+  'en.dc_cci.aetna_broker_and_emprs_hours' => 'from 8am-5pm EST, Monday - Friday',
+  'en.dc_cci.aetna_sbe' => 'For Small Business Employees',
+  'en.dc_cci.aetna_sbe_call' => 'Call 1-855-885-3289',
+  'en.dc_cci.aetna_href_sbe_call' => 'tel:+18558853289',
+  'en.dc_cci.aetna_sbe_hours' => 'from 8am-6pm EST, Monday - Friday',
+  'en.dc_cci.aetna_congressional_employees' => 'For Congressional Employees',
+  'en.dc_cci.aetna_congressional_employees_phone' => '1-800-544-0526',
+  'en.dc_cci.aetna_congressional_employees_href_phone' => 'tel:+18005440526',
+  'en.dc_cci.aetna_congressional_employees_hours' => 'from 8am-8pm EST, Monday - Friday',
+
+  'en.dc_cci.bcbs_ivl' => 'For Individual/Non-Group:',
+  'en.dc_cci.bcbs_small' => 'For Small Group/FOM:',
+  'en.dc_cci.bcbs_ivl_phone' => '1-800-822-2700',
+  'en.dc_cci.bcbs_href_ivl_phone' => 'tel:+18008222700',
+  'en.dc_cci.bcbs_small_phone' => '1-800-782-3675',
+  'en.dc_cci.bcbs_small_href_phone' => 'tel:+18007823675',
+  'en.dc_cci.bcbs_email' => 'send secure email after login to member account',
+  'en.dc_cci.bcbs_hours' => 'Monday through Friday, 8:00 a.m. to 6:00 p.m. Eastern Time',
+
+  'en.dc_cci.bestlife_phone' => '1-800-433-0088',
+  'en.dc_cci.bestlife_href_phone' => 'tel:+18004330088',
+
+  'en.dc_cci.cf_phone' => '1-855-444-3121',
+  'en.dc_cci.cf_href_phone' => 'tel:+18554443121',
+  'en.dc_cci.cf_congressional' => 'Note: Congressional Employees may also contact Carefirst at:',
+  'en.dc_cci.cf_congressional_phone' => '1-855-792-2587',
+  'en.dc_cci.cf_congressional_href_phone' => 'tel:+18557922587',
+
+  'en.dc_cci.dd_phone_1' => '1-800-471-0236',
+  'en.dc_cci.dd_href_phone_1' => 'tel:+18004710236',
+  'en.dc_cci.dd_phone_2' => '1-800-471-0275',
+  'en.dc_cci.dd_href_phone_2' => 'tel:+18004710275',
+
+  'en.dc_cci.dentegra_phone' => '1-800-471-0284',
+  'en.dc_cci.dentegra_href_phone' => 'tel:+18004710284',
+
+  'en.dc_cci.dominion_phone' => '1-855-224-3016',
+  'en.dc_cci.dominion_href_phone' => 'tel:+18552243016',
+
+  'en.dc_cci.kaiser_phone' => '1-800-777-7902',
+  'en.dc_cci.kaiser_href_phone' => 'tel:+18007777902',
+  'en.dc_cci.kaiser_congressional' => 'Congressional Employees may also contact Kaiser Permanente at:',
+  'en.dc_cci.kaiser_congressional_phone' => '1-877-852-4737',
+  'en.dc_cci.kaiser_congressional_href_phone' => 'tel:+18778524737',
+
+  'en.dc_cci.metlife_phone' => '1-855-638-2221',
+  'en.dc_cci.metlife_href_phone' => 'tel:+18556382221',
+
+  'en.dc_cci.uh_phone' => '1-877-856-2430',
+  'en.dc_cci.uh_href_phone' => 'tel:18778562430',
+}
