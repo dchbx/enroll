e8445227f05f2790ed7b010f5623d4846019aaac
Bill Transue <transue@gmail.com>
2020-04-09 12:49:01 -0400

    Try again

---
+++ cucumber_booster_config/lib/cucumber_booster_config/version.rb
@@ -0,0 +1,3 @@
+module CucumberBoosterConfig
+  VERSION = "1.4.1".freeze
+end
---
+++ cucumber_booster_config/spec/cucumber_booster_config_spec.rb
@@ -0,0 +1,7 @@
+require 'spec_helper'
+
+describe CucumberBoosterConfig do
+  it 'has a version number' do
+    expect(CucumberBoosterConfig::VERSION).not_to be nil
+  end
+end
