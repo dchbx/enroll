c91b8897c00f49c110f39a760b4dd403327b6e30
RyanEddyIC <ryan.eddy@ideacrew.com>
2020-10-28 16:04:47 -0400

    Add begin coverage checks for 2/1 and 3/1 effective on enrollments

--- app/models/services/ivl_enrollment_service.rb
+++ app/models/services/ivl_enrollment_service.rb
@@ -46,10 +46,34 @@ module Services
         :kind.in => ['individual', 'coverall'],
         :aasm_state.in => ['auto_renewing', 'renewing_coverage_selected']
       )
-      @logger.info "Total IVL auto renewing enrollment count: #{ivl_enrollments.count}"
-      count = 0
-      ivl_enrollments.no_timeout.each do |enrollment|
-        if enrollment.may_begin_coverage?
+      ivl_enrollments_2_1 = HbxEnrollment.where(
+        :effective_on => current_benefit_period.start_on + 1.month,
+        :kind.in => ['individual', 'coverall'],
+        :aasm_state.in => ['auto_renewing', 'renewing_coverage_selected']
+      )
+      ivl_enrollments_3_1 = HbxEnrollment.where(
+        :effective_on => current_benefit_period.start_on + 2.months,
+        :kind.in => ['individual', 'coverall'],
+        :aasm_state.in => ['auto_renewing', 'renewing_coverage_selected']
+      )
+      begin
+        @logger.info "Total IVL auto renewing enrollment count: #{ivl_enrollments.count}"
+        count = 0
+        ivl_enrollments.no_timeout.each do |enrollment|
+          if enrollment.may_begin_coverage?
+            enrollment.begin_coverage!
+            count += 1
+            @logger.info "Processed enrollment: #{enrollment.hbx_id}"
+          end
+        end
+        ivl_enrollments_2_1.no_timeout.each do |enrollment|
+          next unless enrollment.may_begin_coverage?
+          enrollment.begin_coverage!
+          count += 1
+          @logger.info "Processed enrollment: #{enrollment.hbx_id}"
+        end
+        ivl_enrollments_3_1.no_timeout.each do |enrollment|
+          next unless enrollment.may_begin_coverage?
           enrollment.begin_coverage!
           count += 1
           @logger.info "Processed enrollment: #{enrollment.hbx_id}"
