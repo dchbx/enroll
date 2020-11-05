f7ee2d2ea7d8f3e072ddf06946caec07e2a309ec
saikumar9 <saikumarkotagiri9@gmail.com>
2020-11-05 12:45:12 -0500

    Refs #90818, rubocop fixes for formatted code

--- components/benefit_sponsors/spec/models/benefit_sponsors/benefit_applications/benefit_application_spec.rb
+++ components/benefit_sponsors/spec/models/benefit_sponsors/benefit_applications/benefit_application_spec.rb
@@ -1306,6 +1306,7 @@ module BenefitSponsors
                           :benefit_sponsorship => benefit_sponsorship,
                           :aasm_state => 'enrollment_ineligible',
                           :effective_period => (predecessor_application.effective_period.min - 1.year)..(predecessor_application.effective_period.min.prev_day))
+
       end
 
       before do
