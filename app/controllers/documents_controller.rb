7a0430993d51b7e1da18d5c6ae59d003f066bfed
harshaellanki <h_vardhan@yahoo.com>
2020-12-16 17:15:50 -0500

    fixed few dangerous sends

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -202,10 +202,6 @@ class DocumentsController < ApplicationController
     end
   end
 
-  def download_employer_document
-    send_file params[:path]
-  end
-
   def download_documents
     docs = Document.find(params[:ids])
     docs.each do |doc|
