dd77303f543ec937edcc5c199fa79c87133a1300
RyanEddyIC <ryan.eddy@ideacrew.com>
2021-02-18 14:08:29 -0500

    Logic for Kaiser pay now initial, payments, and other carrier. Relevant glossary fixes

--- app/assets/javascripts/glossary.js
+++ app/assets/javascripts/glossary.js
@@ -19,18 +19,15 @@ function runGlossary() {
     var terms = [
       // UPDATE TEXT OF TERMS AFTER DROPDOWN TEXT REFACTOR
       {
-        // "term": "View my coverage details",
-        "term": "View Details",
+        "term": "View my coverage details",
         "description": "Learn more about what my plan covers."
       },
       {
-        // "term": "Make changes to my coverage",
-        "term": "Make Changes",
+        "term": "Make changes to my coverage",
         "description": "Make changes to my plan."
       },
       {
-        // "term": "Make a first payment for my new plan",
-        "term": "Make a first Payment for a new plan",
+        "term": "Make a first payment for my new plan",
         "description": "This payment will confirm your enrollment and allow you to begin your coverage. Please note it typically takes up to 3-5 business days after you sign up for you to be able to make a first payment."
       },
       // Kaiser payment definition
@@ -1123,7 +1120,7 @@ function runGlossary() {
           var newElement   = "";
           $(matchingEl).html().toString().split(popoverRegex).forEach(function(text){
             // in the case of the pay now actions dropdown, if kaiser enrollment use kaiser definition, if any other use other definition
-            if (term = "Make payments for my plan") {
+            if (term = "Make Payments for My Plan") {
               if ($(matchingEl).hasClass('pay-now-other')) {
                 description = "Go to your insurance company's payment portal to make a new payment or manage your existing payments.";
               }
